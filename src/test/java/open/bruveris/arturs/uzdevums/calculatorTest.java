ebe8d3f0f2b2998cbadb7a85a7eedbe39a45cc21
Kristaps Baumanis <kristaps8894@gmail.com>
2022-04-19 23:32:20 +0300

    changed assertions

--- src/test/java/open/bruveris/arturs/uzdevums/calculatorTest.java
+++ src/test/java/open/bruveris/arturs/uzdevums/calculatorTest.java
@@ -1,25 +1,25 @@
 package open.bruveris.arturs.uzdevums;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 
 public class calculatorTest {
     @org.junit.jupiter.api.Test
     public void sumTest() {
-        Assert.assertEquals(3, calculator.sum(1,2));
+        Assertions.assertEquals(3, calculator.sum(1,2));
     }
 
     @org.junit.jupiter.api.Test
     public void subTest() {
-        Assert.assertEquals(3, calculator.sub(6,3));
+        Assertions.assertEquals(3, calculator.sub(6,3));
     }
 
     @org.junit.jupiter.api.Test
     public void multTest() {
-        Assert.assertEquals(3, calculator.multiply(1,3));
+        Assertions.assertEquals(3, calculator.multiply(1,3));
     }
 
     @org.junit.jupiter.api.Test
     public void divTest() {
-        Assert.assertEquals(3, calculator.div(9,3));
+        Assertions.assertEquals(3, calculator.div(9,3));
     }
 }
