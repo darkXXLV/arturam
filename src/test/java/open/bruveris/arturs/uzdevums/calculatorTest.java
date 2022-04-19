86694fc1e2a5fa2d3b0d073d74769fcacc25cbf0
Kristaps Baumanis <kristaps8894@gmail.com>
2022-04-19 23:28:48 +0300

    chenged tests

--- src/test/java/open/bruveris/arturs/uzdevums/calculatorTest.java
+++ src/test/java/open/bruveris/arturs/uzdevums/calculatorTest.java
@@ -3,22 +3,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class calculatorTest {
-    @Test
+    @org.junit.jupiter.api.Test
     public void sumTest() {
         Assert.assertEquals(3, calculator.sum(1,2));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void subTest() {
         Assert.assertEquals(3, calculator.sub(6,3));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void multTest() {
         Assert.assertEquals(3, calculator.multiply(1,3));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void divTest() {
         Assert.assertEquals(3, calculator.div(9,3));
     }
